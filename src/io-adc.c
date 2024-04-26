d08fe1bbdc984f1b40a4d4e37e91bf3fb28c990a
Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>
2024-04-27 00:14:36 +0900

    Enable two ADC channels

--- src/io-adc.c
+++ src/io-adc.c
@@ -14,21 +14,31 @@ static const struct adc_channel_cfg ch_cfg = {
 	.gain = ADC_GAIN_1,
 	.reference = ADC_REF_INTERNAL,
 	.acquisition_time = ADC_ACQ_TIME_DEFAULT,
-	.channel_id = 0,
+	.channel_id = 1,
 	.differential = false,
 };
 
-static uint16_t g_buf[4];
-static struct adc_sequence sequence0 = {
-	.buffer = &g_buf[3],
-	.buffer_size = sizeof(g_buf[3]),
-	.channels = 1 << 0,	//Ch.0
-	.resolution = 12,	// 12bit AD
-	.calibrate = false,
-	.oversampling = false,
-	.options = NULL,
+static uint16_t g_ad_buffer[2][4];
+static struct adc_sequence sequence[] = {
+	[0]{
+		.buffer = &g_ad_buffer[0][3],
+		.buffer_size = sizeof(g_ad_buffer[0][3]),
+		.channels = 1 << 0,	//Ch.0
+		.resolution = 12,	// 12bit AD
+		.calibrate = false,
+		.oversampling = false,
+		.options = NULL,
+	},
+	[1] {
+		.buffer = &g_ad_buffer[1][3],
+		.buffer_size = sizeof(g_ad_buffer[1][3]),
+		.channels = 1 << 1,	//Ch.1
+		.resolution = 12,	// 12bit AD
+		.calibrate = false,
+		.oversampling = false,
+		.options = NULL,
+	},
 };
-
 //-------------------------------------------------------------
 static int io_adc_init(const struct device *const adc_dev)
 {
@@ -57,21 +67,40 @@ void io_adc_static_thread(void* arg1, void* arg2, void* arg3)
 
 	(void)io_adc_init(g_adc_dev);
 
-	int x = 0;
     while (1) {
 		uint32_t value = 0;
 
-		ret = adc_read(g_adc_dev, &sequence0);
+		ret = adc_read(g_adc_dev, &sequence[0]);
 		if (ret < 0) {
 			LOG_ERR("adc_read 0 ret: %d", ret);
 			goto do_return;
 		}
 
-		value = ((uint32_t)g_buf[0] + (uint32_t)g_buf[1] + (uint32_t)g_buf[2] + (uint32_t)g_buf[3] + 2) / 4;
+		value = ((uint32_t)g_ad_buffer[0][0]
+				 + (uint32_t)g_ad_buffer[0][1]
+				 + (uint32_t)g_ad_buffer[0][2]
+				 + (uint32_t)g_ad_buffer[0][3]
+				 + 2) / 4;
 		LOG_INF("adc ch0 value: %d", value);
-		g_buf[0] = g_buf[1];
-		g_buf[1] = g_buf[2];
-		g_buf[2] = g_buf[3];
+		g_ad_buffer[0][0] = g_ad_buffer[0][1];
+		g_ad_buffer[0][1] = g_ad_buffer[0][2];
+		g_ad_buffer[0][2] = g_ad_buffer[0][3];
+
+		ret = adc_read(g_adc_dev, &sequence[1]);
+		if (ret < 0) {
+			LOG_ERR("adc_read 1 ret: %d", ret);
+			goto do_return;
+		}
+
+		value = ((uint32_t)g_ad_buffer[1][0]
+				 + (uint32_t)g_ad_buffer[1][1]
+				 + (uint32_t)g_ad_buffer[1][2]
+				 + (uint32_t)g_ad_buffer[1][3]
+				 + 2) / 4;
+		LOG_INF("adc ch1 value: %d", value);
+		g_ad_buffer[1][0] = g_ad_buffer[1][1];
+		g_ad_buffer[1][1] = g_ad_buffer[1][2];
+		g_ad_buffer[1][2] = g_ad_buffer[1][3];
 
         k_usleep(250 * 1000);
     }
